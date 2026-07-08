72dc26e655fbdaa98537794b6ba6fe9f3e0b65a7
neil-lobo <neil_edward_lobo@hotmail.com>
2026-07-08 19:47:35 -0400

    convert lua to ts

--- spec/misc.spec.ts
+++ spec/misc.spec.ts
@@ -1,69 +1,10 @@
-import { fs } from "../src/fs";
 import { randomId } from "../src/id";
 
-describe("log tests", () => {
-  it("logs levels", () => {
-    c2.log(c2.LogLevel.Debug, "debug");
-    c2.log(c2.LogLevel.Info, "info");
-    c2.log(c2.LogLevel.Warning, "warning");
-    c2.log(c2.LogLevel.Critical, "critical");
-  });
+describe("id tests", () => {
+  it("randomId length", () => {
+    const len = 16;
+    const id = randomId(len);
 
-  it("multiple strings", () => {
-    c2.log(c2.LogLevel.Debug, "foo", "bar", "baz");
-  });
-
-  it("lists", () => {
-    c2.log(c2.LogLevel.Debug, [1, 2, 3], [4, 5, 6]);
-  });
-
-  it("objects", () => {
-    c2.log(
-      c2.LogLevel.Debug,
-      {
-        id: 1,
-        foo: "bar",
-      },
-      {
-        id: 2,
-        foo: "baz",
-      },
-    );
-  });
-});
-
-describe("fs tests", () => {
-  const rawSettings = `
-{
-  "foo": "bar",
-  "baz": {
-    "fizz": "buzz"
-  },
-  "arr": [1, 2, 3],
-  "objs": [
-    {
-      "id": 1,
-      "body": "fizzbuzz"
-    },
-    {
-      "id": 2,
-      "body": "ipsum"
-    }
-  ]
-}  
-`;
-
-  it("write", () => {
-    fs.write("data_test/settings.json", rawSettings);
-  });
-
-  it("read", () => {
-    fs.read("data_test/settings.json");
-  });
-});
-
-describe("misc tests", () => {
-  it("random id", () => {
-    c2.log(c2.LogLevel.Debug, randomId(16));
+    assert.equals(len, id.length);
   });
 });
--- src/fs.ts
+++ src/fs.ts
@@ -1,7 +1,39 @@
-// @ts-ignore
-import * as impl from "./lua/fs";
+export type WriteMode = "w" | "a";
 
-export const fs: {
-  read: (filename: string) => string;
-  write: (filename: string, data: string) => void;
-} = impl;
+export function writeFile(
+  path: string,
+  mode: string,
+  data: string,
+): LuaMultiReturn<[undefined] | [undefined, string]> {
+  const [file, errStr] = io.open(path, mode);
+
+  if (!file) {
+    return $multi(undefined, errStr);
+  }
+
+  const [written, writtenErrStr] = file.write(data);
+  if (!written) {
+    return $multi(undefined, writtenErrStr);
+  }
+
+  file.close();
+
+  return $multi(undefined);
+}
+
+export function readFile(
+  path: string,
+): LuaMultiReturn<[string] | [undefined, string]> {
+  const [file, errStr] = io.open(path, "r");
+
+  if (!file) {
+    return $multi(undefined, errStr);
+  }
+
+  const data = file.read("a");
+  if (!data) {
+    return $multi(undefined, "Read no data");
+  }
+
+  return $multi(data);
+}
