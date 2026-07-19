550c546b9d9866b54d4ecdbcc68d129a11202004
neil-lobo <neil_edward_lobo@hotmail.com>
2026-07-19 14:50:42 -0400

    remove old jsdoc comment

--- src/command.ts
+++ src/command.ts
@@ -44,10 +44,6 @@ export class Command<T extends string> {
     }
   }
 
-  /**
-   * @throws {CommandError}
-   * @throws {Error}
-   */
   async run(
     ctx: CommandContext,
     args: string[],
